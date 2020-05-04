3a854db8619f565ed55f5c6e5e066487b000c0f1
gsankara <20753500+gsankara@users.noreply.github.com>
2020-05-03 20:59:51 -0700

    Update FactAppViewModel.java

--- App/Android/app/src/main/java/com/fact19/factapp/FactAppViewModel.java
+++ App/Android/app/src/main/java/com/fact19/factapp/FactAppViewModel.java
@@ -4,16 +4,7 @@ import androidx.lifecycle.ViewModel;
 
 public class FactAppViewModel extends ViewModel {
 
-    public int gargleScheduleTime = 0;
     public boolean handTrackerStatus = false;
-
-    public int getGargleScheduleTime() {
-        return gargleScheduleTime;
-    }
-
-    public void setGargleScheduleTime(int gargleScheduleTime) {
-        this.gargleScheduleTime = gargleScheduleTime;
-    }
 
     public boolean isHandTrackerStatus() {
         return handTrackerStatus;
